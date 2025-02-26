0d5a0bf0b092c2dbe03e8c00fe538040ffe7a365
Mateusz <Mateusz@DESKTOP-VH3M7DS>
2025-02-26 22:45:06 +0100

    First column finished.

--- RenPyReader/Components/Pages/FileHandler.razor.cs
+++ RenPyReader/Components/Pages/FileHandler.razor.cs
@@ -1,4 +1,7 @@
-﻿using RenPyReader.Components.Shared;
+﻿using Microsoft.AspNetCore.Components.Forms;
+using RenPyReader.Components.Shared;
+using Microsoft.JSInterop;
+using System.IO.Compression;
 
 namespace RenPyReader.Components.Pages
 {
@@ -7,5 +10,88 @@ namespace RenPyReader.Components.Pages
         private FileSizeHandler? _fileSizeHandler;
 
         private FilePropertyHandler? _filePropertyHandler;
+
+        private FileMemoryUsageHandler? _fileMemoryUsageHandler;
+
+        private InputFile? _inputFileReference;
+
+        private IBrowserFile? _selectedFile;
+
+        private string? _errorMessage;
+
+        private bool _isWorking;
+
+        private List<ZipArchiveEntry>? _zipEntries;
+
+        private List<string>? _zipEntriesNames;
+
+        private void HandleInputFile(InputFileChangeEventArgs e)
+        {
+            _selectedFile = null;
+            IBrowserFile file = e.File;
+
+            if (file == null)
+            {
+                _errorMessage = "No file was uploaded.";
+                StateHasChanged();
+                return;
+            }
+
+            if (!Path.GetExtension(file.Name).Equals(".zip", StringComparison.OrdinalIgnoreCase))
+            {
+                _errorMessage = "File must be zip.";
+                StateHasChanged();
+                return;
+            }
+
+            _selectedFile = file;
+            _filePropertyHandler?.SetFile(file);
+            StateHasChanged();
+        }
+
+        private async Task Scan()
+        {
+            _isWorking = true;
+            _fileMemoryUsageHandler!.Start();
+            StateHasChanged();
+
+            try
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    var maxFileSize = _fileSizeHandler!
+                        .GetMaximumSizeBytes();
+                    await _selectedFile!
+                        .OpenReadStream(maxFileSize)
+                        .CopyToAsync(memoryStream);
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+
+                    using (var archive = new ZipArchive(
+                        memoryStream, ZipArchiveMode.Read))
+                    {
+                        _zipEntriesNames = 
+                            [.. archive.Entries.Select(x => x.Name)];
+                    }
+                }
+            }
+            finally
+            {
+                _isWorking = false;
+                _fileMemoryUsageHandler!.Stop();
+                StateHasChanged();
+            }
+        }
+
+        private async Task Process()
+        {
+
+        }
+
+        private bool IsFileSelected => _selectedFile != null && !_isWorking;
+
+        private async Task TriggerFileInputAsync()
+        {
+            await JSRuntime.InvokeVoidAsync("triggerInputFile", "inputFileID");
+        }
     }
 }
--- RenPyReader/Components/Shared/FileMemoryUsageHandler.razor.cs
+++ RenPyReader/Components/Shared/FileMemoryUsageHandler.razor.cs
@@ -1,7 +1,48 @@
-﻿namespace RenPyReader.Components.Shared
+﻿using Microsoft.AspNetCore.Components;
+using System.Diagnostics;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace RenPyReader.Components.Shared
 {
     public partial class FileMemoryUsageHandler
     {
+        private Timer? timer;
 
+        private long memoryUsage;
+
+        public event Action<long> MemoryUsageUpdated = delegate { };
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                timer = new Timer(1000);
+                timer.Elapsed += OnTimerElapsed;
+            }
+        }
+
+        public void Start()
+        {
+            timer?.Start();
+        }
+
+        public void Stop()
+        {
+            timer?.Stop();
+        }
+
+        private void OnTimerElapsed(object? sender, ElapsedEventArgs e)
+        {
+            memoryUsage = Process.GetCurrentProcess().PrivateMemorySize64;
+            MemoryUsageUpdated?.Invoke(memoryUsage);
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void Dispose()
+        {
+            timer?.Stop();
+            timer?.Dispose();
+        }
     }
 }
