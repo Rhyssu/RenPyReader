052a619f142e4464c0852046dd001ceb1af02969
Mateusz Rzepnicki <mtzp@demant.com>
2025-03-09 12:43:13 +0100

    Added pretty okay filtering to DatabaseBrowser.

--- RenPyReader/Components/Pages/DatabaseBrowser.razor.cs
+++ RenPyReader/Components/Pages/DatabaseBrowser.razor.cs
@@ -1,6 +1,8 @@
-﻿namespace RenPyReader.Components.Pages
+﻿using Microsoft.AspNetCore.Components;
+
+namespace RenPyReader.Components.Pages
 {
-    public partial class DatabaseBrowser
+    public partial class DatabaseBrowser : ComponentBase
     {
         private List<string> _tableNames = new();
 
@@ -9,6 +11,8 @@
         private string _selectedTableName = string.Empty;
 
         private List<Dictionary<string, string>> _tableData = new();
+
+        private List<Dictionary<string, string>> _filteredTableData = new();
 
         private bool _isWorking;
 
@@ -22,6 +26,7 @@
                 try
                 {
                     _tableNames = await SQLiteService.GetTableNamesAsync();
+                    _tableNames = _tableNames.Where(tn => !tn.Contains("documents")).ToList();
                 }
                 catch (Exception ex)
                 {
@@ -43,6 +48,7 @@
             try
             {
                 _tableData = await SQLiteService.GetTableDataAsync(tableName);
+                _filteredTableData = _tableData;
             }
             catch (Exception ex)
             {
@@ -52,6 +58,32 @@
             {
                 _isWorking = false;
                 StateHasChanged();
+            }
+        }
+
+        private void OnFilterTextChanged(ChangeEventArgs e, string columnName)
+        {
+            var filterText = e.Value?.ToString() ?? string.Empty;
+            if (string.IsNullOrWhiteSpace(filterText))
+            {
+                _filteredTableData = _tableData;
+            }
+            else
+            {
+                bool isNumericColumn = _tableData.FirstOrDefault()?.ContainsKey(columnName) == true 
+                    && double.TryParse(_tableData.First()[columnName], out _);
+                if (isNumericColumn)
+                {
+                    _filteredTableData = _tableData.Where(row => row[columnName]?
+                    .Equals(filterText, StringComparison.OrdinalIgnoreCase) == true)
+                    .ToList();
+                }
+                else
+                {
+                    _filteredTableData = _tableData.Where(row => row[columnName]?
+                    .Contains(filterText, StringComparison.OrdinalIgnoreCase) == true)
+                    .ToList();
+                }
             }
         }
     }
